86bd9e501d8431ac9819c08248bb87a5f997788d
rlanvin <rlanvin@3a531f15-fc10-3c5a-ef65-789bdcfddca7>
2010-04-14 08:42:16 +0000

    Adding MIT license.

--- trunk/lib/GoogleChartAxis.class.php
+++ trunk/lib/GoogleChartAxis.class.php
@@ -2,6 +2,12 @@
 
 /**
  * This file is part of GoogleChart PHP library.
+ *
+ * Copyright (c) 2010 Rémi Lanvin <remi@cloudconnected.fr>
+ *
+ * Licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE file.
  */
 
 /**
