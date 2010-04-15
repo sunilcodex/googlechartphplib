c3b74d9c5fa4ecccde7401ba6c9788b04bdd3d10
rlanvin <rlanvin@3a531f15-fc10-3c5a-ef65-789bdcfddca7>
2010-04-15 13:42:26 +0000

    Adding require.

--- lib/GoogleChart.class.php
+++ lib/GoogleChart.class.php
@@ -10,6 +10,9 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
+require 'GoogleChartData.class.php';
+require 'GoogleChartAxis.class.php';
+
 /**
  * A chart.
  *
